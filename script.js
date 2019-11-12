29b9b50f78e757f844f1bc9e1de493aafe60aef7
Joe Paul <joe.paul0712@gmail.com>
2019-11-12 11:44:04 +0000

    add query selctors

---
+++ script.js
@@ -0,0 +1,10 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+function randomTime(min, max) {
+
+}
