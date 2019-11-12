d85ee67e1f9b549834a35cce54700a178b75ba05
Joe Paul <joe.paul0712@gmail.com>
2019-11-12 11:52:49 +0000

    added functions

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
-const moles = document.querySelectorAll('.mole');
+const pandas = document.querySelectorAll('.panda');
 let lastHole;
 let timeUp = false;
 let score = 0;
 
 function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const idx = Math.floor(Math.random() * holes.length);
+  const holes = holes[idx];
 
 }
+
+function peep() {
+
+}
+
+function startGame() {
+
+}
+
+function bonk(e) {
+
+}
+
+pandas.forEach(panda => panda.addEventListener('click', bonk))
